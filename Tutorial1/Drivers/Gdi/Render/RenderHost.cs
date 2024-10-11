c52f9488debd0790113c7622459519d71e01c399
SLHORTKY <salih-ort@hotmail.com>
2024-10-11 20:31:39 +0300

    back buffer implemented

--- Tutorial1/Drivers/Gdi/Render/RenderHost.cs
+++ Tutorial1/Drivers/Gdi/Render/RenderHost.cs
@@ -5,41 +5,105 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Tutorial1.Engine.Render;
+using Tutorial1.Utils;
 
 namespace Tutorial1.Drivers.Gdi.Render
 {
     public class RenderHost : Engine.Render.RenderHost
     {
         private Graphics GraphicsHost { get; set; }
-        private BufferedGraphics bufferedGraphics { get; set; } 
+        private BufferedGraphics BufferedGraphics { get; set; } 
+        private IntPtr GraphicsHostDeviceContext { get; set; }
+        private DirectBitmap BackBuffer { get; set; }
         private Font curFont { get; set; }
 
         public RenderHost(IRenderHostSetup setup) : base(setup)
         {
             GraphicsHost = Graphics.FromHwnd(setup.HostHandle);
-          
-            bufferedGraphics = BufferedGraphicsManager.Current.Allocate(GraphicsHost, new Rectangle(Point.Empty, W.GetClientRectangle(setup.HostHandle).Size));
-
+            GraphicsHostDeviceContext = GraphicsHost.GetHdc();
+            CreateBuffer(BufferSize);
+            CreateViewPort(ViewPortSize);
+            
             curFont = new Font("Consolas", 12);
         }
 
         public override void Dispose()
         {
-            bufferedGraphics.Dispose();
-            bufferedGraphics = default;
+            GraphicsHost.ReleaseHdc(GraphicsHostDeviceContext);
+            GraphicsHostDeviceContext = default;
 
             GraphicsHost.Dispose();
             GraphicsHost = default;
 
+            DisposeBuffer();
+            DisposeViewPort();
             base.Dispose();
+        }
+
+        protected override void ResizeBuffer(Size size)
+        {
+            base.ResizeBuffer(size);
+            DisposeBuffer();
+            CreateBuffer(size);
+        }
+
+        protected override void ResizeViewPort(Size size)
+        {
+            base.ResizeViewPort(size);
+            DisposeViewPort();
+            CreateViewPort(size);
+        }
+
+        private void CreateBuffer(Size size)
+        {
+            BackBuffer = new DirectBitmap(size);
+
+        }
+        private void DisposeBuffer()
+        {
+            BackBuffer.Dispose();
+            BackBuffer = default;
+        }
+
+        private void CreateViewPort(Size size)
+        {
+            BufferedGraphics = BufferedGraphicsManager.Current.Allocate(GraphicsHostDeviceContext, new Rectangle(Point.Empty,size));
+        }
+        private void DisposeViewPort()
+        {
+            BufferedGraphics.Dispose();
+            BufferedGraphics = default;
         }
 
         protected override void RenderInternal()
         {
-            bufferedGraphics.Graphics.Clear(Color.Black);
-            bufferedGraphics.Graphics.DrawString($"{FpsCounter.FpsRender}", curFont , Brushes.Red , 0, 0);
+            var graphics = BackBuffer.Graphics;
+            graphics.Clear(Color.Black);
 
-            bufferedGraphics.Render();
+            var t = DateTime.UtcNow.Millisecond / 1000.0;
+            Color GetColor(int x, int y) => Color.FromArgb
+            (
+                byte.MaxValue,
+                (byte)((double)x / BufferSize.Width * byte.MaxValue),
+                (byte)((double)y / BufferSize.Height * byte.MaxValue),
+                (byte)(Math.Sin(t * Math.PI) * byte.MaxValue)
+            );
+
+           
+
+            Parallel.For(0, BackBuffer.Buffer.Length, index =>
+            {
+                BackBuffer.GetXY(index, out var x , out var y);
+                //BackBuffer.SetPixel(x, y, GetColor(x, y));
+                BackBuffer.Buffer[index] = GetColor(x,y).ToArgb();
+            });
+
+           
+            graphics.DrawString($"{FpsCounter.FpsRender}", curFont , Brushes.Red , 0, 0);
+
+            BufferedGraphics.Graphics.DrawImage(BackBuffer.Bitmap, new RectangleF(PointF.Empty, ViewPortSize),new RectangleF(PointF.Empty, BufferSize),GraphicsUnit.Pixel);
+
+            BufferedGraphics.Render(GraphicsHostDeviceContext);
         }
 
     }
--- Tutorial1/Engine/Render/RenderHost.cs
+++ Tutorial1/Engine/Render/RenderHost.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,17 +14,28 @@ namespace Tutorial1.Engine.Render
 
         public IInput HostInput { get; private set; }
 
+        protected Size BufferSize { get; private set; }
+
+        protected Size ViewPortSize { get; private set; }
+
         public FpsCounter FpsCounter { get; private set; }
 
         protected RenderHost(IRenderHostSetup setup)
         {
             HostHandle = setup.HostHandle;
             HostInput = setup.HostInput;
+            BufferSize = HostInput.Size;
+            ViewPortSize = HostInput.Size;
+
             FpsCounter = new FpsCounter(new TimeSpan(0,0,0,0,1000));
+
+            HostInput.SizeChanged += HostInputOnSizeChanged;
         }
 
         public virtual void Dispose()
         {
+            HostInput.SizeChanged -= HostInputOnSizeChanged;
+
             FpsCounter.Dispose();
             FpsCounter = default;
 
@@ -31,6 +43,26 @@ namespace Tutorial1.Engine.Render
             HostInput = default;
 
             HostHandle = default;
+        }
+
+        private void HostInputOnSizeChanged(object sender, ISizeEventArgs args)
+        {
+            var size = args.NewSize;
+            if(size.Width < 1 || size.Height < 1)
+            {
+                size = new Size(1, 1);
+            }
+
+            ResizeBuffer(size);
+            ResizeViewPort(size);
+        }
+        protected virtual void ResizeBuffer(Size size)
+        {
+            BufferSize = size;
+        }
+        protected virtual void ResizeViewPort(Size size)
+        {
+            ViewPortSize = size;
         }
 
         public void Render()
